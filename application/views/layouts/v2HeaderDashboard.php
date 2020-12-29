b1ea22c453b3908338d00f95c2f73a279dae6924
Angel Gonzalez <desarrollomovil@vendty.com>
2020-12-29 17:31:57 -0500

    debug instance

--- application/views/layouts/v2HeaderDashboard.php
+++ application/views/layouts/v2HeaderDashboard.php
@@ -32,8 +32,9 @@
     //imagenes a utilizar
     $cimagenes = &get_instance();
     $cimagenes->load->model('crm_imagenes_model');
-    /*$imagenes = $cimagenes->crm_imagenes_model->imagenes();
-    if(count($imagenes) > 0 && empty($this->session->userdata('new_imagenes')))
+    $imagenes = $cimagenes->crm_imagenes_model->imagenes();
+    echo count($imagenes); 
+    /*if(count($imagenes) > 0 && empty($this->session->userdata('new_imagenes')))
         $this->session->set_userdata('new_imagenes',$imagenes);*/
 ?>
 
